9092f9e08b14e1bf42b574b32a42c9e65302b44d
mio suzuki <wonjongah@gmail.com>
2021-01-06 21:59:32 +0900

    2021-01-06-2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# DataStructure_CodingTest
+#### 2021-01-06 ~
+
+
+
+- coding_teat -> 이것이 취업을 위한 코딩 테스트다 with 파이썬
+- data_structure -> 윤성우의 열혈 자료구조
---
+++ data_structure/chapter01/LinearSearch.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int LSearch(int ar[], int len, int target){
+    
+    int i;
+    for(i = 0; i < len; i++){
+        if(ar[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(void){
+    int arr[] = {3, 5, 2, 4, 9};
+    int idx;
+
+    idx = LSearch(arr, sizeof(arr)/sizeof(int), 4);
+    if(idx == -1){
+        printf("Å½»ö ½ÇÆĞ\n");
+    }else{
+        printf("Å¸°Ù ÀúÀå ÀÎµ¦½º : %d \n", idx);
+    }
+
+    idx = LSearch(arr, sizeof(arr)/sizeof(int), 7);
+    if(idx == -1){
+        printf("Å½»ö ½ÇÆĞ\n");
+    }else{
+        printf("Å¸°Ù ÀúÀå ÀÎµ¦½º : %d \n", idx);
+    }
+
+    return 0;
+}
