fa39d6b88af213e4c3ed696a373d38b14eacc19b
mio suzuki <wonjongah@gmail.com>
2021-02-16 23:31:28 +0900

    2021-02-16

---
+++ baekjoon/LEVEL01/LEVEL01.py
@@ -0,0 +1,19 @@
+# 2557 Hello World! 출력하기
+
+print("Hello World!")
+
+# 10718 We love kriii
+
+print("강한친구 대한육군\n" * 2)
+# print("강한친구 대한육군")
+# print("강한친구 대한육군")
+
+# 10171 고양이
+
+print("\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
+
+# 10712 개
+
